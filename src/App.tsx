15bf9b566eea8b0093fd2cf3a6d223aaced4dab9
Zhexuan Ma <zhexuanma@gmail.com>
2025-01-19 00:31:54 +0800

    init tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 className="text-3xl font-extralight text-purple-300 underline gray">
+          Hello world!
+        </h1>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -29,7 +23,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
