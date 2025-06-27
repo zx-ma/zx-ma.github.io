dfdcbd708dee65b391ff794a0c970abaf3a8ccc0
Zhexuan Ma <zhexuanma@gmail.com>
2025-06-27 15:12:04 +0800

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
+import { Button } from "@/components/ui/button";
+
 function App() {
   const [count, setCount] = useState(0);
 
@@ -21,6 +23,9 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <Button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
